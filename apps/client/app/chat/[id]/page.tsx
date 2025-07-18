c9879099a96d3fc6dfbe2cbe5d35df9a9d70fc88
nawinsharma <nawinsharma60@gmail.com>
2025-07-19 00:54:42 +0530

    Add chat page component for dynamic chat group display
    - Introduced a new `page.tsx` file for rendering chat groups based on dynamic IDs.
    - Implemented fetching of chat group details, users, and messages with error handling for invalid IDs.
    - Integrated `ChatBase` component to display chat group information and messages.

---
+++ apps/client/app/chat/[id]/page.tsx
@@ -0,0 +1,28 @@
+import ChatBase from "@/components/chat/ChatBase";
+import { fetchChats } from "@/fetch/chatsFetch";
+
+import { fetchChatGroup, fetchChatGroupUsers } from "@/fetch/groupFetch";
+import { MessageType } from "@/type";
+import { GroupChatUserType } from "@/type";
+import { GroupChatType } from "@/type";
+import { notFound } from "next/navigation";
+import React from "react";
+
+export default async function chat(props: { params: Promise<{ id: string }> }) {
+  const { id } = await props.params;
+  if (id.length !== 36) {
+    return notFound();
+  }
+  const chatGroup: GroupChatType | null = await fetchChatGroup(id);
+  if (chatGroup === null) {
+    return notFound();
+  }
+  const chatGroupUsers: Array<GroupChatUserType> | [] = await fetchChatGroupUsers(id);
+  const chats: Array<MessageType> | [] = await fetchChats(id);
+
+  return (
+    <div>
+      <ChatBase group={chatGroup} users={chatGroupUsers} oldMessages={chats} />
+    </div>
+  );
+}
